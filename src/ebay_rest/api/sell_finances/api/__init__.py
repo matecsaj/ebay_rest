2aa264161c3101fa9bad4d055e2acc2b53f0544f
matecsaj <matecsaj@gmail.com>
2026-02-15 14:35:50 -0500

    Removed deprecated models and API for eBay Marketing: `Category`, `ItemLocation`, `MostWatchedItem`, and `MostWatchedItemsApi`.
    Bumped version number.

--- src/ebay_rest/api/sell_finances/api/__init__.py
+++ src/ebay_rest/api/sell_finances/api/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
+from ...sell_finances.api.billing_activity_api import BillingActivityApi
 from ...sell_finances.api.payout_api import PayoutApi
 from ...sell_finances.api.seller_funds_summary_api import SellerFundsSummaryApi
 from ...sell_finances.api.transaction_api import TransactionApi
